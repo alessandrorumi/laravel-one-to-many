f587a2067c536e7faa008643f23b42d12d5d9e2f
alessandrorumi <rumi.alessandro21@gmail.com>
2024-02-22 16:13:05 +0100

    Add Style index

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,24 +1,31 @@
 @extends('layouts.main-layout')
+
 @section('head')
     <title>Home</title>
 @endsection
+
 @section('content')
-    <h1>Tipologia e Progetti</h1>
-    <ul>
-        @foreach ($types as $type)
-            <li>
-                <div class="type mt-5">
-                    <h3>Tipologia: {{ $type->name }}</h3>
-                    <ul>
-                        @foreach ($type->projects  as $project)
-                            <li>
-                                <b>Progetto:</b><span>
-                                {{ $project->name }}</span>
-                            </li>
-                        @endforeach
-                    </ul>
+    <div class="container mt-5">
+        <h1 class="text-center">Tipologia e Progetti</h1>
+        <div class="row">
+            @foreach ($types as $type)
+                <div class="col-md-6">
+                    <div class="card mb-4">
+                        <div class="card-body">
+                            <h3 class="card-title">Tipologia: {{ $type->name }}</h3>
+                            <ul class="list-group list-group-flush">
+                                @foreach ($type->projects as $project)
+                                    <li class="list-group-item">
+                                        <h5 class="card-subtitle mb-2 text-muted">Progetto: {{ $project->name }}</h5>
+                                        <p class="card-text">Descrizione: {{ $project->description }}</p>
+                                        <p class="card-text">Autore: {{ $project->author }}</p>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-            </li>
-        @endforeach
-    </ul>
+            @endforeach
+        </div>
+    </div>
 @endsection
